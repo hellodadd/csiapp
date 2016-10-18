6c75fec22de0f3586cb8b7660ba5c791a40c495a
Joan Lin <joan.lin@mesmotech.com>
2016-10-18 13:13:31 +0800

    解析PC端初始化App資料

--- app/src/main/java/com/android/csiapp/XmlHandler/XmlHandler.java
+++ app/src/main/java/com/android/csiapp/XmlHandler/XmlHandler.java
@@ -13,6 +13,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlSerializer;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ import java.util.List;
 
 public class XmlHandler {
 
+    private final static String TAG = "XmlHandler";
     private void createDeviceMsgXmlFile(Object obj) {
         XmlSerializer xmlSerializer = null;
         FileOutputStream fileOutputStream = null;
@@ -34,7 +36,6 @@ public class XmlHandler {
             File file = new File(Environment.getExternalStorageDirectory(), "DeviceMsg.xml");
             fileOutputStream = new FileOutputStream(file);
 
-
             String encoding = "utf-8";
             xmlSerializer.setOutput(fileOutputStream, encoding);
             xmlSerializer.startDocument(encoding, null);
@@ -43,7 +44,6 @@ public class XmlHandler {
 
             xmlSerializer.startTag(null, "device");
             changeLine(xmlSerializer, enter);
-
 
             //内容结点
             for(Device d: devices) {
@@ -73,7 +73,6 @@ public class XmlHandler {
             //根节点结束
             xmlSerializer.endTag(null, "device");
             xmlSerializer.endDocument();
-            Log.d("aaaa", "endDocument");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -105,6 +104,102 @@ public class XmlHandler {
         device.setMapVersion(mapversion);
         devices.add(device);
         createDeviceMsgXmlFile(devices);
+    }
 
+    public Object[] getInitialDeviceCmd() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "InitDeviceCmd.xml");
+            FileInputStream fileInputStream = new FileInputStream(file);
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setInput(fileInputStream, "utf-8");
+            int eventType = parser.getEventType();
+            Dictionary dictionary =null;
+            User user = null;
+            List<Dictionary> dictionarys = null;
+            List<User> users = null;
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        String name = parser.getName();
+                        if (name.equalsIgnoreCase("dictionarys")) {
+                            dictionarys = new ArrayList<Dictionary>();
+                        } else if (name.equalsIgnoreCase("dictionary")) {
+                            dictionary = new Dictionary();
+                        } else if (name.equalsIgnoreCase("dictkey")) {
+                            eventType = parser.next();
+                            dictionary.setDictKey(parser.getText());
+                        } else if (name.equalsIgnoreCase("parentkey")) {
+                            eventType = parser.next();
+                            dictionary.setParentKey(parser.getText());
+                        } else if (name.equalsIgnoreCase("rootkey")) {
+                            eventType = parser.next();
+                            dictionary.setRootKey(parser.getText());
+                        } else if (name.equalsIgnoreCase("dictvalue")) {
+                            eventType = parser.next();
+                            dictionary.setDictValue(parser.getText());
+                        } else if (name.equalsIgnoreCase("dictremark")) {
+                            eventType = parser.next();
+                            dictionary.setDictRemark(parser.getText());
+                        } else if (name.equalsIgnoreCase("dictspell")) {
+                            eventType = parser.next();
+                            dictionary.setDictSpell(parser.getText());
+                        } else if (name.equalsIgnoreCase("users")) {
+                            users = new ArrayList<User>();
+                        } else if (name.equalsIgnoreCase("user")) {
+                            user = new User();
+                        } else if (name.equalsIgnoreCase("loginname")) {
+                            eventType = parser.next();
+                            user.setLoginName(parser.getText());
+                        } else if (name.equalsIgnoreCase("password")) {
+                            eventType = parser.next();
+                            user.setPassword(parser.getText());
+                        } else if (name.equalsIgnoreCase("username")) {
+                            eventType = parser.next();
+                            user.setUserName(parser.getText());
+                        } else if (name.equalsIgnoreCase("unitcode")) {
+                            eventType = parser.next();
+                            user.setUnitCode(parser.getText());
+                        } else if (name.equalsIgnoreCase("unitname")) {
+                            eventType = parser.next();
+                            user.setUnitName(parser.getText());
+                        } else if (name.equalsIgnoreCase("idcardno")) {
+                            eventType = parser.next();
+                            user.setIdCardNo(parser.getText());
+                        } else if (name.equalsIgnoreCase("contact")) {
+                            eventType = parser.next();
+                            user.setContact(parser.getText());
+                        } else if (name.equalsIgnoreCase("duty")) {
+                            eventType = parser.next();
+                            user.setDuty(parser.getText());
+                        }
+
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if(parser.getName().equalsIgnoreCase("dictionary")) {
+                            if (dictionarys!=null) {
+                                dictionarys.add(dictionary);
+                            }
+                            dictionary = null;
+                        } else if(parser.getName().equalsIgnoreCase("user")) {
+                            if (users != null) {
+                                users.add(user);
+                            }
+                            user = null;
+                        }
+                        break;
+                }
+
+                eventType = parser.next();
+
+            }
+            fileInputStream.close();
+            return new Object[]{dictionarys, users};
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
