cde1887bfdc400a6b6e17e9ed2b998e53e298415
Joan Lin <joan.lin@mesmotech.com>
2016-11-09 15:31:34 +0800

    PC<->Android 更換地圖命令

--- app/src/main/java/com/android/csiapp/PcSocketTransmission/ThreadReadWriterIOSocket.java
+++ app/src/main/java/com/android/csiapp/PcSocketTransmission/ThreadReadWriterIOSocket.java
@@ -1,15 +1,19 @@
 package com.android.csiapp.PcSocketTransmission;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Environment;
 import android.util.Log;
 
+import com.android.csiapp.Crime.setting.ZipUtils;
+import com.android.csiapp.RestartService;
 import com.android.csiapp.XmlHandler.DataInitial;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
@@ -21,6 +25,8 @@ import java.net.Socket;
 public class ThreadReadWriterIOSocket implements Runnable {
 
     public static final String TAG = "ThreadRWIOSocket";
+    String mMapPath = Environment.getExternalStorageDirectory()+"/amapsdk/offlineMap/data";
+    String mMapCachePath = Environment.getExternalStorageDirectory()+"/Csibackup/Amap.zip";
     private Socket client;
     private Context context;
 
@@ -34,6 +40,7 @@ public class ThreadReadWriterIOSocket implements Runnable {
         Log.d(TAG, "A client has connected to server!");
         BufferedOutputStream out;
         BufferedInputStream in;
+        boolean result = false;
         try {
             /* PC端发来的数据msg */
             String currCMD = "";
@@ -59,11 +66,10 @@ public class ThreadReadWriterIOSocket implements Runnable {
                     DataInitial dataInitial = new DataInitial(context);
                     /* 根据命令分别处理数据 */
                     if (currcmdinfo != null) {
+                        Log.d(TAG, "Command = "+currcmdinfo[1]);
                         switch (currcmdinfo[1]) {
                             case 1: //获取设备信息命令
                                 dataInitial.createDeviceMsgXml();
-
-                                receiveDataFromSocket(in, currcmdinfo);
                                 File file = FileHelper.newFile("DeviceMsg.xml");
                                 if (file.exists() == true) {
                                     byte[] abyte = FileHelper.readFile(file);
@@ -94,10 +100,24 @@ public class ThreadReadWriterIOSocket implements Runnable {
                             case 3: //App更新命令
                                 break;
                             case 4: //地图更新命令
+                                Log.d(TAG, "Map Update!!");
+                                byte[] filebytes = receiveDataFromSocketByte(in, currcmdinfo);
+                                File Uzfile = new File(mMapCachePath);
+                                try {
+                                    ZipUtils.upZipFile(Uzfile, mMapPath);
+                                    deleteFiles(Uzfile);
+                                    result = true;
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                concatCmdline(out, currcmdinfo, 1);
+                                sendResult(out, result);
+                                SocketService.ioThreadFlag=false;
+
                                 break;
                             case 11: //获取现场列表命令
                                 //組成BaseMsg.xml
-                                boolean result = dataInitial.CreateBaseMsg();
+                                result = dataInitial.CreateBaseMsg();
 
                                 //获取BaseMsg.xml
                                 receiveDataFromSocket(in, currcmdinfo);
@@ -197,6 +217,7 @@ public class ThreadReadWriterIOSocket implements Runnable {
                 Log.e(TAG, Thread.currentThread().getName() + "---->" + "read write error333333");
                 e.printStackTrace();
             }
+            restartApp();
         }
     }
 
@@ -208,17 +229,33 @@ public class ThreadReadWriterIOSocket implements Runnable {
         int[] cmdinfo = new int[4]; //int[0]:CmdId, int[1]:Cmd, int[2]:is File, int[3]:Data Length
         byte[] cmdidbytes = new byte[4];
         byte[] datalength = new byte[4];
+        int ch = 0;
+        int count = 0;
+        int COMMAND_LENGTH=10;
+        byte[] commandbytes = new byte[10];
+
+        try {
+            while ((ch = in.read()) != -1) {
+                Log.d(TAG,"ch="+ch);
+                commandbytes[count] =(byte) ch;
+                count++ ;
+                if (count == COMMAND_LENGTH) {
+                    Log.d(TAG,"Command Received Finished");
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
 
-            int numReadedBytes = in.read(tempbuffer, 0, tempbuffer.length);
-
             /* cmdline:10byte, byte 1~4:Cmd, byte 5:Cmd, byte 6:is File, byte 7~10:Data Length*/
-            System.arraycopy(tempbuffer,0,cmdidbytes,0,4);
+            System.arraycopy(commandbytes,0,cmdidbytes,0,4);
             cmdinfo[0] = Utils.bytesToInt(cmdidbytes);
-            cmdinfo[1] = tempbuffer[4];
-            cmdinfo[2] = tempbuffer[5];
-            System.arraycopy(tempbuffer,6,datalength,0,4);
+            cmdinfo[1] = commandbytes[4];
+            cmdinfo[2] = commandbytes[5];
+            System.arraycopy(commandbytes,6,datalength,0,4);
             cmdinfo[3] = Utils.bytesToInt(datalength);
 
             for(int i=0;i<4;i++) {
@@ -229,8 +266,6 @@ public class ThreadReadWriterIOSocket implements Runnable {
             e.printStackTrace();
             cmdinfo = null;
         }
-        // Log.v(Service139.TAG, "msg=" + msg);
-        // tempbuffer =null;
         return cmdinfo;
     }
 
@@ -261,6 +296,39 @@ public class ThreadReadWriterIOSocket implements Runnable {
 
         return receiveString;
 
+    }
+    public byte[] receiveDataFromSocketByte(BufferedInputStream in, int[] cmdinfo) {
+        int ch = 0;
+        int count = 0;
+        int receivedatalength = cmdinfo[3];
+        Log.d(TAG, "Revived Data Length =" + receivedatalength);
+
+        byte[] filebytes = null;
+
+        int pos = 0;
+        int rcvLen =0;
+        try {
+            String path = mMapCachePath;
+            filebytes = new byte[receivedatalength];
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
+
+            int bytesRead = in.read(filebytes,0,filebytes.length);
+            int current = bytesRead;
+
+            do {
+                bytesRead = in.read(filebytes, current, (filebytes.length-current));
+                if(bytesRead >= 0) current += bytesRead;
+            } while(current < receivedatalength);
+            bos.write(filebytes, 0 , current);
+
+            Log.d(TAG,"Received Data Finished");
+            bos.flush();
+            bos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filebytes;
     }
 
     public void sendDeviceinfo(BufferedOutputStream out, int[] cmdinfo, File file) {
@@ -342,5 +410,30 @@ public class ThreadReadWriterIOSocket implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void deleteFiles(File file) {
+        if (file.isFile()) {
+            file.delete();
+            return;
+        }
+
+        if(file.isDirectory()){
+            File[] childFiles = file.listFiles();
+            if (childFiles == null || childFiles.length == 0) {
+                file.delete();
+                return;
+            }
+
+            for (int i = 0; i < childFiles.length; i++) {
+                deleteFiles(childFiles[i]);
+            }
+            file.delete();
+        }
+    }
+
+    public void restartApp() {
+        Intent mServiceIntent = new Intent(context, RestartService.class);
+        context.startService(mServiceIntent);
     }
 }
